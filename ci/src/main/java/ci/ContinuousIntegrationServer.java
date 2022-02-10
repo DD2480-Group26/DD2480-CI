f41755026a68c73ab7d4ff4176fed322b9221215
Julia Byström <juliavasastan@gmail.com>
2022-02-10 06:19:11 +0100

    feat create handler for incoming http request solving #25

--- ci/src/main/java/ci/ContinuousIntegrationServer.java
+++ ci/src/main/java/ci/ContinuousIntegrationServer.java
@@ -3,6 +3,7 @@ package ci;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 
 import java.io.IOException;
 
@@ -10,23 +11,42 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
 /**
  Skeleton of a ContinuousIntegrationServer which acts as webhook
  See the Jetty documentation for API documentation of those classes.
  */
-public class ContinuousIntegrationServer extends AbstractHandler
-{
+public class ContinuousIntegrationServer extends AbstractHandler {
     public void handle(String target,
                        Request baseRequest,
                        HttpServletRequest request,
                        HttpServletResponse response)
-            throws IOException, ServletException
-    {
+            throws IOException, ServletException {
+        String githubEvent = request.getHeader("X-Github-Event");
+
+        if(!githubEvent.equals("")){
+            switch(githubEvent){
+                case "push":
+                    // DO on push actions
+                    System.out.println("Push");
+                    break;
+                case "issues":
+                    // DO issues action
+                    System.out.println("Issues");
+                default:
+                    // DO default actions
+                    System.out.println("No event match for " + githubEvent );
+                    break;
+            }
+
+        }
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
 
-        System.out.println(target);
 
         // here you do all the continuous integration tasks
         // for example
@@ -34,6 +54,44 @@ public class ContinuousIntegrationServer extends AbstractHandler
         // 2nd compile the code
 
         response.getWriter().println("CI job done");
+    }
+
+    /**
+     * Parse request data and create a string containing the payload.
+     *
+     * @param request A HttpServletRequest recieved by handler.
+     * @return String Payload of the request.
+     */
+    public String getRequestPayload( HttpServletRequest request){
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = null;
+
+        try {
+            InputStream inputStream = request.getInputStream();
+            if (inputStream != null) {
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                char[] charBuffer = new char[128];
+                int bytesRead = -1;
+                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                    // Append charvuffer to String builder with given offsets.
+                    stringBuilder.append(charBuffer, 0, bytesRead);
+                }
+            } else {
+                stringBuilder.append("");
+            }
+        } catch (IOException ex) {
+            System.out.println("Error when parsing payload")
+        } finally {
+            try {
+                bufferedReader.close();
+            } catch (IOException ex) {
+                throw ex;
+            }
+
+        }
+
+        return stringBuilder.toString();
+
     }
 
     // used to start the CI server in command line
