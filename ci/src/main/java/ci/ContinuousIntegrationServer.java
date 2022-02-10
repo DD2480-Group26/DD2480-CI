4c441df9693c9377dfa23cd5a4728251751ec8ca
Julia <juliavasastan@gmail.com>
2022-02-10 14:04:13 +0100

    feat create handler for incoming http request solving #25 (#29)
    * feat create handler for incoming http request solving #25
    
    * solved issue #30, extract the branch name from the payload
    
    * refactor unecesarry if statement for event switch for handler in #25
    
    Co-authored-by: massyv <victor.massy@grenoble-inp.org>

--- ci/src/main/java/ci/ContinuousIntegrationServer.java
+++ ci/src/main/java/ci/ContinuousIntegrationServer.java
@@ -3,6 +3,7 @@ package ci;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,23 +17,54 @@ import org.eclipse.jgit.api.PullCommand;
 import org.eclipse.jgit.api.PullResult;
 import org.eclipse.jgit.merge.MergeStrategy;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
 /**
  * Skeleton of a ContinuousIntegrationServer which acts as webhook
  * See the Jetty documentation for API documentation of those classes.
  */
 public class ContinuousIntegrationServer extends AbstractHandler {
 
-
     public void handle(String target,
                        Request baseRequest,
                        HttpServletRequest request,
                        HttpServletResponse response)
             throws IOException, ServletException {
+
+        String githubEvent = request.getHeader("X-Github-Event");
+        switch(githubEvent){
+            case "push":
+                //Isolate the branchName from the payload
+                String payload = getRequestPayload(request);
+                String issue = "issue%2F";
+                if(!payload.contains(issue)) System.out.println("Wrong branch name");
+                int branchIdx = payload.indexOf(issue) + issue.length();
+                String branchName = "issue/";
+                while(! (payload.charAt(branchIdx) == (char) '%')) {
+                    branchName+= payload.charAt(branchIdx);
+                    branchIdx ++;
+                }
+
+                System.out.println("Push");
+                break;
+            case "issues":
+                // DO issues action
+                System.out.println("Issues");
+            default:
+                // DO default actions
+                System.out.println("No event match for " + githubEvent );
+                break;
+        }
+
+
+
+
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
 
-        System.out.println(target);
 
 
         // here you do all the continuous integration tasks
@@ -51,6 +83,44 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         git.getRepository().close();
         GitConnector.deleteDirectory(localDirectory);
         localDirectory.delete();
+    }
+
+    /**
+     * Parse request data and create a string containing the payload.
+     *
+     * @param request A HttpServletRequest recieved by handler.
+     * @return String Payload of the request.
+     */
+    public String getRequestPayload( HttpServletRequest request){
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = null;
+
+        try {
+            InputStream inputStream = request.getInputStream();
+            if (inputStream != null) {
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                char[] charBuffer = new char[128];
+                int bytesRead = -1;
+                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                    // Append charvuffer to String builder with given offsets.
+                    stringBuilder.append(charBuffer, 0, bytesRead);
+                }
+            } else {
+                stringBuilder.append("");
+            }
+        } catch (IOException ex) {
+            System.out.println("Error when parsing payload");
+        } finally {
+            try {
+                bufferedReader.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+        }
+
+        return stringBuilder.toString();
+
     }
 
     // used to start the CI server in command line
