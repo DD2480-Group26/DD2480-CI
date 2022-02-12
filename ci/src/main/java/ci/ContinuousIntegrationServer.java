0fb75092c90fe5b8022532b4676d86b7613d0ed2
ElisabethC <elisabeth.chen010@gmail.com>
2022-02-12 16:36:25 +0100

    doc #51 add appropriate JavaDoc and comments to ContinuousIntegrationServer, left some undone part

--- ci/src/main/java/ci/ContinuousIntegrationServer.java
+++ ci/src/main/java/ci/ContinuousIntegrationServer.java
@@ -3,7 +3,6 @@ package ci;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,71 +10,67 @@ import java.io.IOException;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.PullCommand;
-import org.eclipse.jgit.api.PullResult;
-import org.eclipse.jgit.merge.MergeStrategy;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.InputStream;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.InputStream;
-import java.lang.ProcessBuilder;
-import java.util.Enumeration;
 import org.json.*;
+
 /**
- * Skeleton of a ContinuousIntegrationServer which acts as webhook
- * See the Jetty documentation for API documentation of those classes. ok
+ * Creates a CI server which acts as a webhook. To run the CI server, use
+ * "./gradlew run". If it shows not permitted then run "chmod 777 gradlew"
+ * before. 
+ * 
+ * To set the webhook, open another terminal window, then run "./ngrok http 8080" 
+ * to get an unique URL (e.g. http://8929b010.ngrok.io). Create a webhook in 
+ * GitHub and add this URL.
+ * 
+ * The CI server handles compiling and testing the pushed code. All participants
+ * will be notified about the CI result with an email.
  */
 public class ContinuousIntegrationServer extends AbstractHandler {
 
     public void handle(String target,
-                       Request baseRequest,
-                       HttpServletRequest request,
-                       HttpServletResponse response)
+            Request baseRequest,
+            HttpServletRequest request,
+            HttpServletResponse response)
             throws IOException, ServletException {
 
         String githubEvent = request.getHeader("X-Github-Event");
         System.out.println(githubEvent);
-        switch(githubEvent){
+        switch (githubEvent) {
             case "push":
-            	
-            	JSONObject payload = new JSONObject(request.getParameter("payload"));
-            	JSONObject headCommit = (JSONObject) payload.get("head_commit");
-            	JSONObject author = (JSONObject) headCommit.get("author");
-            	
-            	
-            	String branchName = (String) payload.get("ref");
-            	branchName = branchName.replaceAll("refs/heads/", "");
-            	String id = (String) headCommit.get("id");
-            	String email = (String) author.get("email");
-            	
-            	
+                // get information from the HTTP request
+                JSONObject payload = new JSONObject(request.getParameter("payload"));
+                JSONObject headCommit = (JSONObject) payload.get("head_commit");
+                JSONObject author = (JSONObject) headCommit.get("author");
 
-                // here you do all the continuous integration tasks
-                // for example
-                // 1st clone your repository
-                PushTester pushTester = new PushTester();
+                String branchName = (String) payload.get("ref");
+                branchName = branchName.replaceAll("refs/heads/", "");
+                String id = (String) headCommit.get("id");
+                String email = (String) author.get("email");
 
+                // TODO: add what is does here
+                PushTester pushTester = new PushTester();   // TODO: why two pushTester?
+                
                 File localDirectory = new File("GitPull/");
-
+                
+                // clone repository
                 Git git = GitConnector.cloneRepo("https://github.com/DD2480-Group26/DD2480-CI.git", localDirectory);
                 GitConnector.gitPull(localDirectory, branchName);
                 GitConnector.checkoutToBranch(localDirectory, "origin/" + branchName);
 
+                // compile and test the code
                 PushTester pt = new PushTester();
                 PushStatus pushStatus = pt.getPushStatus(localDirectory);
 
-                // 2nd compile the code
                 pushTester.fileExecuter(localDirectory);
 
                 response.getWriter().println("CI job Done");
 
-                //Delete the directory
+                // Delete the directory
                 git.getRepository().close();
                 GitConnector.deleteDirectory(localDirectory);
                 localDirectory.delete();
@@ -85,20 +80,13 @@ public class ContinuousIntegrationServer extends AbstractHandler {
                 System.out.println("Issues");
             default:
                 // DO default actions
-                System.out.println("No event match for " + githubEvent );
+                System.out.println("No event match for " + githubEvent);
                 break;
         }
-
-
-
 
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
-
-
-
-
 
     }
 
@@ -108,7 +96,7 @@ public class ContinuousIntegrationServer extends AbstractHandler {
      * @param request A HttpServletRequest recieved by handler.
      * @return String Payload of the request.
      */
-    public String getRequestPayload( HttpServletRequest request){
+    public String getRequestPayload(HttpServletRequest request) {
         StringBuilder stringBuilder = new StringBuilder();
         BufferedReader bufferedReader = null;
 
@@ -140,9 +128,6 @@ public class ContinuousIntegrationServer extends AbstractHandler {
 
     }
 
-
-
-
     // used to start the CI server in command line
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
@@ -150,7 +135,6 @@ public class ContinuousIntegrationServer extends AbstractHandler {
 
         server.start();
         server.join();
-
 
     }
 }
