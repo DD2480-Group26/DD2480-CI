f7c6bcaab153c9bb8baeff08faedce71d1616ed1
carlenge <carl.engelhardt@gmail.com>
2022-02-15 19:05:36 +0100

    solves #57. removes unused imports in each code file

--- ci/src/main/java/ci/PushTester.java
+++ ci/src/main/java/ci/PushTester.java
@@ -2,10 +2,7 @@ package ci;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.InputStream;
-import java.lang.ProcessBuilder;
 import java.io.File;
-import java.io.IOException;
 
 public class PushTester  {
     private Boolean compileSuccess = true;
