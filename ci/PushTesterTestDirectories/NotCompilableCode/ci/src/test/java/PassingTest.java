d189ad05261e88ca79bfa0621928a0b5909548ad
Julia Byström <juliavasastan@gmail.com>
2022-02-15 23:57:04 +0100

    documented tests for test

--- ci/PushTesterTestDirectories/NotCompilableCode/ci/src/test/java/PassingTest.java
+++ ci/PushTesterTestDirectories/NotCompilableCode/ci/src/test/java/PassingTest.java
@@ -2,10 +2,13 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+/*
+ * Dummy test code used for testing dictionary with passing test
+ */
 public class PassingTest {
 
     @Test
-    public void Test1() {
+    public void PassingTest() {
         assertEquals(1, 1);
     }
 }
