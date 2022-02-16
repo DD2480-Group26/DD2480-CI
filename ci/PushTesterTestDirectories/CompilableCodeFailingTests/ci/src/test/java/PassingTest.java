58c4e238af74f83323c6d6aab839ab5e8d334aa7
carlenge <carl.engelhardt@gmail.com>
2022-02-16 16:21:37 +0100

    solves #81.  removes submodule CompilableCodeFailingTests and replaces with correct files.

---
+++ ci/PushTesterTestDirectories/CompilableCodeFailingTests/ci/src/test/java/PassingTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/*
+* Dummy test code used for testing dictionary with passing test
+*/
+public class PassingTest {
+
+    @Test
+    public void passingTest() {
+        assertEquals(2, 1);
+    }
+}
